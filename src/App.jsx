98e6ba0477374d6f40e98c21c99fbd697e2a9f28
maks-rusnak <alineitgirl@yandex.com>
2026-01-28 12:30:18 +0300

    feat: add general task to repo

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Button from "./components/Button";
+
+const App = () => {
+
+  return (
+    <>
+      <div>
+        <Button title="Вы должны увидеть этот текст при наведении на кнопку">Кнопка</Button>
+        <Button title="Это уже другая кнопка">Это уже другая кнопка</Button>
+      </div>
+    </> 
+  )
+}
+
+export default App;
